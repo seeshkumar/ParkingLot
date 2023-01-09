b23e9c45f5d11109ea5ab19523a4e9c1702a2603
seeshkumar <eeshkumar.s@Technovert.com>
2023-01-09 12:28:00 +0530

    Implemented TicketService

---
+++ Services/TicketService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using ParkingLot.Interfaces;
+using ParkingLot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingLot.Injectors;
+
+namespace ParkingLot.Services
+{
+    class TicketService : ITicketService
+    {
+
+        public List<Ticket> ReadTickets()
+        {
+            string json = File.ReadAllText("./assets/Tickets.json");
+            return JsonConvert.DeserializeObject<List<Ticket>>(json);
+        }
+
+        public Boolean SaveTickets(List<Ticket> tickets)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(tickets);
+                File.WriteAllText("./assets/Tickets.json", json);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        void ITicketService.DeleteTicket(Injector injector, List<Ticket> tickets, Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        Ticket ITicketService.GenerateTicket(Injector injector, Slot freeSlot, Vechile vechile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
