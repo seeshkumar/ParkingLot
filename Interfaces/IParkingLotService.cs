5b1f50666c0338d4b709b3a1e3af3cb13610ea79
seeshkumar <eeshkumar.s@Technovert.com>
2023-01-09 11:44:45 +0530

    added Interfaces

---
+++ Interfaces/IParkingLotService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingLot.Models;
+using ParkingLot.Injectors;
+
+namespace ParkingLot.Interfaces
+{
+    interface IParkingLotService
+    {
+        public List<Slot> ReadSlots();
+        public Boolean SaveSlots(List<Slot> slots);
+        public List<Slot> InitializeLot(Injector injector, int twoWheelerSlots, int fourWheelerSlots, int heavyVechileSlots);
+
+        public string ParkVechile(Injector injector, List<Slot> slots, Vechile vechile);
+        public string UnParkVechile(Injector injector, List<Slot> slots, string number);
+    }
+}
---
+++ Interfaces/ITicketService.cs
@@ -0,0 +1,20 @@
+﻿using ParkingLot.Injectors;
+using ParkingLot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot.Interfaces
+{
+    interface ITicketService
+    {
+        public List<Ticket> ReadTickets();
+        public Boolean SaveTickets(List<Ticket> tickets);
+        public Ticket GenerateTicket(Injector injector, Slot freeSlot, Vechile vechile);
+        public void DeleteTicket(Injector injector, List<Ticket> tickets, Ticket ticket);
+
+    }
+}
