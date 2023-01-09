1e38e5060e3536f53c3196d1e8d38c2d1ffbdd5b
seeshkumar <eeshkumar.s@Technovert.com>
2023-01-09 11:35:11 +0530

    added Models

---
+++ Models/Slot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot.Models
+{
+    class Slot
+    {
+        public string name { get; set; }
+        public string category { get; set; }
+        public Boolean occupied { get; set; }
+
+
+        public Slot(string name, string category, bool occupied)
+        {
+            this.name = name;
+            this.category = category;
+            this.occupied = occupied;
+        }
+    }
+}
---
+++ Models/Vechile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot.Models
+{
+    class Vechile
+    {
+        public string number { get; set; }
+        public string category { get; set; }
+
+        public Vechile(string number, string category)
+        {
+            this.number = number;
+            this.category = category;
+        }
+    }
+}
